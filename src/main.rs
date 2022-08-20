c8ae2c18d878714e1d7150d7451b451c5d6d6bbe
Martin Disch <martindisch@gmail.com>
2022-08-20 12:38:35 +0200

    Test glob crate

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,13 @@ fn main() -> Result<()> {
         .nth(1)
         .ok_or_else(|| eyre!("Missing argument target directory"))?;
 
+    let directory = format!("{directory}/**/*.tsx");
     println!("{directory}");
+
+    let files = glob::glob(&directory)?.filter_map(Result::ok);
+    for file in files {
+        println!("{}", file.display());
+    }
 
     Ok(())
 }
