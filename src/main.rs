293df1c9b7fb735ecd890d01001230fa679825b3
Martin Disch <martindisch@gmail.com>
2022-08-20 16:08:16 +0200

    Ignore files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use eyre::{eyre, Result};
+use globset::{Glob, GlobSetBuilder};
 use std::env;
 use walkdir::WalkDir;
 
@@ -7,7 +8,17 @@ fn main() -> Result<()> {
         .nth(1)
         .ok_or_else(|| eyre!("Missing argument target directory"))?;
 
-    let files = WalkDir::new(directory).into_iter().filter_map(Result::ok);
+    let mut builder = GlobSetBuilder::new();
+    builder.add(Glob::new("**/node_modules/**")?);
+    builder.add(Glob::new("**/dist/**")?);
+    builder.add(Glob::new("**/global.type.ts")?);
+    builder.add(Glob::new("applications/shop-e2e/**")?);
+    let ignore_set = builder.build()?;
+
+    let files = WalkDir::new(directory)
+        .into_iter()
+        .filter_entry(|e| !ignore_set.is_match(e.path()))
+        .filter_map(Result::ok);
     for file in files {
         println!("{}", file.path().display());
     }
