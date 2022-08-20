c905f8929bd4246f0d62cb9abfdf1ebe7f70cf32
Martin Disch <martindisch@gmail.com>
2022-08-20 16:19:35 +0200

    Filter out everything that doesn't match
    Takes 23 ms to walk sample directory.

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,15 @@ fn main() -> Result<()> {
     builder.add(Glob::new("**/dist")?);
     builder.add(Glob::new("**/global.type.ts")?);
     builder.add(Glob::new("**/applications/shop-e2e")?);
-    let ignore_set = builder.build()?;
+    let ignore_glob = builder.build()?;
+
+    let target_glob = Glob::new("**/*.{ts,tsx}")?.compile_matcher();
 
     let files = WalkDir::new(directory)
         .into_iter()
-        .filter_entry(|e| !ignore_set.is_match(e.path()))
-        .filter_map(Result::ok);
+        .filter_entry(|e| !ignore_glob.is_match(e.path()))
+        .filter_map(Result::ok)
+        .filter(|e| target_glob.is_match(e.path()));
     for file in files {
         println!("{}", file.path().display());
     }
