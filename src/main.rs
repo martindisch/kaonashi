9a944ca7821a2230c0d54229b0e7bef10948d65d
Martin Disch <martindisch@gmail.com>
2022-08-20 15:56:19 +0200

    Prepare testdata & dependencies for filtering

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 use eyre::{eyre, Result};
 use std::env;
+use walkdir::WalkDir;
 
 fn main() -> Result<()> {
     let directory = env::args()
         .nth(1)
         .ok_or_else(|| eyre!("Missing argument target directory"))?;
 
-    let directory = format!("{directory}/**/*.tsx");
-    println!("{directory}");
-
-    let files = glob::glob(&directory)?.filter_map(Result::ok);
+    let files = WalkDir::new(directory).into_iter().filter_map(Result::ok);
     for file in files {
-        println!("{}", file.display());
+        println!("{}", file.path().display());
     }
 
     Ok(())
