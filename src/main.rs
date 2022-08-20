d985d6b478c202f1efba1af581cb4319a0c933ad
Martin Disch <martindisch@gmail.com>
2022-08-20 12:22:53 +0200

    Expect target directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use eyre::{eyre, Result};
+use std::env;
+
+fn main() -> Result<()> {
+    let directory = env::args()
+        .nth(1)
+        .ok_or_else(|| eyre!("Missing argument target directory"))?;
+
+    println!("{directory}");
+
+    Ok(())
 }
